6bf3be9ad21d6f5b0508717905bed9fc18dbe6cd
Jegor Zelenjak <jzelenjak@proton.me>
2024-05-25 18:50:30 +0000

    Update plot_months.py

--- plot_months.py
+++ plot_months.py
@@ -19,49 +19,53 @@ formatter.set_scientific(False)
 
 dateformat = "%Y-%m"
 scale = "linear"
-time, count, amount_btc, amount_usd = [], [], [], []
+months, count, amount_btc, amount_usd = [], [], [], []
 with open(sys.argv[1], "r") as file:
     for line in file.readlines()[1:]:
         parts = line.split(file_separator)
-        time.append(datetime.datetime.strptime(parts[0], dateformat))
+        months.append(datetime.datetime.strptime(parts[0], dateformat))
         count.append(int(parts[1]))
-        amount_btc.append(float(parts[2]))
-        amount_usd.append(float(parts[3]) / 1000000)
+        amount_btc.append(round(float(parts[2]), 2))
+        amount_usd.append(round(float(parts[3]) / 1000000, 2))
 
 
-fig, (ax1, ax2, ax3) = plt.subplots(3)
-fig.suptitle("Timeline of ransom transactions per month")
+fig, axes = plt.subplots(3)
 
-# Plot transactions per time
-ax1.plot(time, count, color='blue')
-ax1.set_title("Number of transactions")
-ax1.xaxis.set_major_formatter(md.DateFormatter('%Y-%m'))
-ax1.xaxis.set_major_locator(md.MonthLocator(interval=3))
-ax1.yaxis.set_major_formatter(formatter)
-ax1.tick_params(labelrotation=25)
-ax1.set_yscale(scale)
-ax1.grid()
+titles = ["Number of transactions", "Payment sum (in BTC)", "Payment sum (in million USD)"]
+stats = [count, amount_btc, amount_usd]
+colours = ["blue", "green", "red"]
+epsilons = [75, 250, 1.5]
 
-# Plot payment sum in BTC per time
-ax2.plot(time, amount_btc, color='green')
-ax2.set_title("Payment sum (in BTC)")
-ax2.xaxis.set_major_formatter(md.DateFormatter('%Y-%m'))
-ax2.xaxis.set_major_locator(md.MonthLocator(interval=3))
-ax2.yaxis.set_major_formatter(formatter)
-ax2.tick_params(labelrotation=25)
-ax2.set_yscale(scale)
-ax2.grid()
+for i in range(len(stats)):
+    ax = axes[i]
+    y_values = stats[i]
 
-# Plot payment sum in BTC per time
-ax3.plot(time, amount_usd, color='red')
-ax3.set_title("Payment sum (in million USD)")
-ax3.xaxis.set_major_formatter(md.DateFormatter('%Y-%m'))
-ax3.xaxis.set_major_locator(md.MonthLocator(interval=3))
-ax3.yaxis.set_major_formatter(formatter)
-ax3.tick_params(labelrotation=25)
-ax3.set_yscale(scale)
-ax3.grid()
+    ax.xaxis.set_major_formatter(md.DateFormatter(dateformat))
+    ax.xaxis.set_major_locator(md.MonthLocator(interval=3))
+    ax.set_xlim(months[0], months[-1])
 
+    ax.yaxis.set_major_formatter(formatter)
+    ax.tick_params(axis='y', labelrotation=25)
+    ax.set_yscale(scale)
+
+    # Add value labels
+    epsilon = epsilons[i]
+    for j in range(1, len(y_values) - 1):
+        if y_values[j] > (y_values[j-1] + epsilon) and y_values[j] > (y_values[j+1] + epsilon):
+            ax.text(months[j], y_values[j], y_values[j], ha="center", va="bottom", fontfamily="monospace", fontsize=11)
+    ax.text(months[0], y_values[0], y_values[0], ha="left", va="bottom", fontfamily="monospace", fontsize=11)
+    ax.text(months[-1], y_values[-1], y_values[-1], ha="right", va="bottom", fontfamily="monospace", fontsize=11)
+
+    ax.plot(months, y_values, color=colours[i], marker='o')
+    ax.set_title(titles[i])
+    ax.grid()
+
+
+first_month = months[0].strftime("%B %Y")
+last_month = months[-1].strftime("%B %Y")
+fig.suptitle(f"Timeline of ransom transactions per month in the period from {first_month} until {last_month}")
+
+fig.autofmt_xdate(rotation=25)
 plt.gcf().set_size_inches(20, 10, forward=True)
 plt.tight_layout()
 
